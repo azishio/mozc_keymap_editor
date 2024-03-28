75ee61f3ab5fcfb7c35faf24fd270472edd7f5a8
azishio <azishio3141@gmail.com>
2024-03-28 14:46:49 +0900

    💄 コマンドの選択UI作成

---
+++ src/components/CommandSelector.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import {
+	type CommandCategory,
+	type EnCommand,
+	commandCategories,
+	commands,
+} from "@/mozc_options";
+import {
+	Check,
+	KeyboardArrowDown,
+	KeyboardArrowRight,
+} from "@mui/icons-material";
+import {
+	Accordion,
+	AccordionDetails,
+	AccordionGroup,
+	AccordionSummary,
+	Box,
+	Chip,
+	List,
+	ListDivider,
+	ListItem,
+	ListItemDecorator,
+	ListSubheader,
+	Option,
+	Select,
+	Stack,
+	Typography,
+	accordionDetailsClasses,
+} from "@mui/joy";
+import { useState } from "react";
+
+export function CommandSelector({
+	initialCommand = null,
+}: { initialCommand?: EnCommand | null }) {
+	const [expand, setExpand] = useState<CommandCategory | null>(
+		initialCommand ? commands[initialCommand].category : null,
+	);
+
+	const [command, setCommand] = useState<null | EnCommand>(null);
+
+	return (
+		<>
+			<Select
+				placeholder={"* 未選択 *"}
+				value={command}
+				renderValue={(selected) => {
+					if (!selected) return null;
+
+					const { category, ja } = commands[selected.value];
+					return (
+						<Typography
+							startDecorator={
+								<Chip component={"span"} variant={"soft"} color={"primary"}>
+									{category}
+								</Chip>
+							}
+						>
+							{ja}
+						</Typography>
+					);
+				}}
+				onChange={(_, newCommand) => setCommand(newCommand)}
+			>
+				<AccordionGroup>
+					<List>
+						{commandCategories.map(({ category, commands }) => (
+							<ListItem nested key={category}>
+								<Accordion
+									expanded={expand === category}
+									onChange={(_, expanded) =>
+										setExpand(expanded ? category : null)
+									}
+								>
+									<ListSubheader
+										sticky
+										variant={expand === category ? "soft" : "plain"}
+										sx={{ paddingY: 0 }}
+									>
+										<AccordionSummary
+											indicator={null}
+											sx={{
+												justifyContent: "start",
+												flexGrow: 1,
+												height: 1,
+												paddingY: 0,
+											}}
+										>
+											<ListItemDecorator>
+												{expand === category ? (
+													<KeyboardArrowDown />
+												) : (
+													<KeyboardArrowRight />
+												)}
+											</ListItemDecorator>
+											<Typography level={"title-md"}>{category}</Typography>
+										</AccordionSummary>
+									</ListSubheader>
+									<AccordionDetails
+										slotProps={{ content: { padding: 0 } }}
+										sx={{
+											[`& .${accordionDetailsClasses.content}`]: {
+												padding: 0,
+											},
+										}}
+									>
+										<List>
+											{commands.map(({ ja, en }, i, l) => (
+												<Box key={en}>
+													<Option value={en} key={en}>
+														<ListItemDecorator>
+															<Check sx={{ opacity: command === en ? 1 : 0 }} />
+														</ListItemDecorator>
+														<Stack>
+															<Typography level={"body-md"}>{ja}</Typography>
+															<Typography level={"body-sm"}>{en}</Typography>
+														</Stack>
+													</Option>
+													<ListDivider />
+												</Box>
+											))}
+										</List>
+									</AccordionDetails>
+								</Accordion>
+							</ListItem>
+						))}
+					</List>
+				</AccordionGroup>
+			</Select>
+		</>
+	);
+}
